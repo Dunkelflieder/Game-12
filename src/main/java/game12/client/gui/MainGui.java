3da50b38f8d59a3dfa339638da5b0b1b5ee589a5
Nerogar <nerogar@arcor.de>
2018-03-06 03:47:13 +0100

    change buttons on the main gui

--- src/main/java/game12/client/gui/MainGui.java
+++ src/main/java/game12/client/gui/MainGui.java
@@ -72,32 +72,38 @@ public class MainGui extends Gui {
 		localButtonTP.setColors(fontColor, fontHoverColor);
 		mainPanel.addElement(localButtonTP, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 100);
 
-		GButton startServerButton = new GButton(font, "start server", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.startServer(Client.ControllerType.FIRST_PERSON)));
-		startServerButton.setColors(fontColor, fontHoverColor);
-		mainPanel.addElement(startServerButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 200);
+		GButton startServerButtonFP = new GButton(font, "start server (first person)", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.startServer(Client.ControllerType.FIRST_PERSON)));
+		startServerButtonFP.setColors(fontColor, fontHoverColor);
+		mainPanel.addElement(startServerButtonFP, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 200);
 
-		GButton connectButton = new GButton(font, "connect local", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.connectLocal(Client.ControllerType.FIRST_PERSON)));
-		connectButton.setColors(fontColor, fontHoverColor);
-		mainPanel.addElement(connectButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 250);
+		GButton startServerButtonTP = new GButton(font, "start server (third person)", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.startServer(Client.ControllerType.THIRD_PERSON)));
+		startServerButtonTP.setColors(fontColor, fontHoverColor);
+		mainPanel.addElement(startServerButtonTP, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 250);
 
-		GButton connectStartButton = new GButton(font, "connect local and start", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.connectStartServer(Client.ControllerType.FIRST_PERSON)));
-		connectStartButton.setColors(fontColor, fontHoverColor);
-		mainPanel.addElement(connectStartButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 300);
+		GButton connectStartButtonFP = new GButton(font, "connect and start (first person)", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.connectStartServer(Client.ControllerType.FIRST_PERSON)));
+		connectStartButtonFP.setColors(fontColor, fontHoverColor);
+		mainPanel.addElement(connectStartButtonFP, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 300);
 
+		GButton connectStartButtonTP = new GButton(font, "connect and start (third person)", BUTTON_SIZE, 40, () -> blendPanelOut(() -> menu.connectStartServer(Client.ControllerType.THIRD_PERSON)));
+		connectStartButtonTP.setColors(fontColor, fontHoverColor);
+		mainPanel.addElement(connectStartButtonTP, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 350);
+
+/*
 		GButton blendOutButton = new GButton(font, "blend out", BUTTON_SIZE, 40, () -> backgroundBlendOut = 0.001f);
 		blendOutButton.setColors(fontColor, fontHoverColor);
-		mainPanel.addElement(blendOutButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 350);
+		mainPanel.addElement(blendOutButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 450);
 
 		GButton resetBlendOutButton = new GButton(font, "reset blend out", BUTTON_SIZE, 40, () -> backgroundBlendOut = 0.0f);
 		resetBlendOutButton.setColors(fontColor, fontHoverColor);
-		mainPanel.addElement(resetBlendOutButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 400);
+		mainPanel.addElement(resetBlendOutButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 500);
+*/
 
 		GButton testPanelButton = new GButton(font, "switch panels", BUTTON_SIZE, 40, () -> {
 			mainBlendDirection = 10;
 			testBlendDirection = -10;
 		});
 		testPanelButton.setColors(fontColor, fontHoverColor);
-		mainPanel.addElement(testPanelButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 500);
+		mainPanel.addElement(testPanelButton, GElement.ALIGNMENT_LEFT, GElement.ALIGNMENT_TOP, BUTTON_OFFSET, 600);
 	}
 
 	private void createTestPanel(Font font, Color fontColor, Color fontHoverColor) {
