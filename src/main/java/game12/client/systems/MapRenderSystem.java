bcf73b5d35f85d424036678840f140028f659a39
Nerogar <nerogar@arcor.de>
2018-03-09 22:35:24 +0100

    add color coding for current and locked rooms

--- src/main/java/game12/client/systems/MapRenderSystem.java
+++ src/main/java/game12/client/systems/MapRenderSystem.java
@@ -5,9 +5,11 @@ import de.nerogar.noise.render.*;
 import de.nerogar.noise.render.deferredRenderer.DeferredContainer;
 import de.nerogar.noise.render.deferredRenderer.DeferredRenderable;
 import de.nerogar.noise.render.deferredRenderer.DeferredRenderer;
+import de.nerogar.noise.util.Color;
 import game12.client.event.BeforeRenderEvent;
 import game12.core.LogicSystem;
 import game12.core.event.MapChangeEvent;
+import game12.core.systems.GameProgressSystem;
 import game12.core.systems.MapSystem;
 
 public class MapRenderSystem extends LogicSystem {
@@ -15,6 +17,9 @@ public class MapRenderSystem extends LogicSystem {
 	private static final float WALL_OFFSET = 0.3f;
 	private static final int   TILE_COUNT  = 8;
 	private static final float TILE_SIZE   = 1f / TILE_COUNT;
+
+	private final Color COLOR_CURRENT_ROOM = new Color(0.4f, 1.0f, 0.1f, 1.0f);
+	private final Color COLOR_LOCKED_ROOM  = new Color(1.0f, 0.2f, 0.0f, 1.0f);
 
 	private EventListener<MapChangeEvent>    mapChangeEventListener;
 	private EventListener<BeforeRenderEvent> beforeRenderEventListener;
@@ -35,7 +40,8 @@ public class MapRenderSystem extends LogicSystem {
 	private DeferredRenderable mapRenderable;
 	private DeferredRenderable backgroundRenderable;
 
-	private MapSystem mapSystem;
+	private MapSystem          mapSystem;
+	private GameProgressSystem gameProgressSystem;
 
 	@Override
 	public void init() {
@@ -53,6 +59,7 @@ public class MapRenderSystem extends LogicSystem {
 		mapShader.deactivate();
 
 		mapSystem = getContainer().getSystem(MapSystem.class);
+		gameProgressSystem = getContainer().getSystem(GameProgressSystem.class);
 
 		addBackgroundMesh();
 	}
@@ -132,6 +139,13 @@ public class MapRenderSystem extends LogicSystem {
 		for (int x = 0; x < mapSystem.getWidth(); x++) {
 			for (int y = 0; y < mapSystem.getHeight(); y++) {
 				int tile = mapSystem.get(x, y);
+				Color color = Color.WHITE;
+
+				if (tile == gameProgressSystem.getCurrentRoom()) {
+					color = COLOR_CURRENT_ROOM;
+				} else if (mapSystem.isRoomLocked(tile)) {
+					color = COLOR_LOCKED_ROOM;
+				}
 
 				if (tile == MapSystem.VOID) {
 					createWall(vertexList, x + 0, y + 0, x + 0, y + 1, 1, 0,
@@ -162,10 +176,10 @@ public class MapRenderSystem extends LogicSystem {
 					float u = 1;
 					float v = 7;
 
-					int indexNN = vertexList.addVertex(x + 0, 0, y + 0, u, v + 1, 1, 1, 1);
-					int indexPN = vertexList.addVertex(x + 1, 0, y + 0, u + 1, v + 1, 1, 1, 1);
-					int indexPP = vertexList.addVertex(x + 1, 0, y + 1, u + 1, v, 1, 1, 1);
-					int indexNP = vertexList.addVertex(x + 0, 0, y + 1, u, v, 1, 1, 1);
+					int indexNN = vertexList.addVertex(x + 0, 0, y + 0, u, v + 1, color.getR(), color.getG(), color.getB());
+					int indexPN = vertexList.addVertex(x + 1, 0, y + 0, u + 1, v + 1, color.getR(), color.getG(), color.getB());
+					int indexPP = vertexList.addVertex(x + 1, 0, y + 1, u + 1, v, color.getR(), color.getG(), color.getB());
+					int indexNP = vertexList.addVertex(x + 0, 0, y + 1, u, v, color.getR(), color.getG(), color.getB());
 
 					vertexList.addIndex(indexNN, indexNP, indexPP);
 					vertexList.addIndex(indexNN, indexPP, indexPN);
@@ -191,10 +205,10 @@ public class MapRenderSystem extends LogicSystem {
 
 		// wall
 		if (!connect) {
-			int index1 = vertexList.addVertex(x0, 0, y0, u, v, 1, 1, 1);
-			int index2 = vertexList.addVertex(x1, 0, y1, u + 1, v, 1, 1, 1);
-			int index3 = vertexList.addVertex(x1 + (dx + x0 - x1) * WALL_OFFSET, 1, y1 + (dy + y0 - y1) * WALL_OFFSET, u + (1 - WALL_OFFSET), v + 1, 1, 1, 1);
-			int index4 = vertexList.addVertex(x0 + (dx + x1 - x0) * WALL_OFFSET, 1, y0 + (dy + y1 - y0) * WALL_OFFSET, u + (WALL_OFFSET), v + 1, 1, 1, 1);
+			int index1 = vertexList.addVertex(x0, 0, y0, u, v, 0, 0, 0);
+			int index2 = vertexList.addVertex(x1, 0, y1, u + 1, v, 0, 0, 0);
+			int index3 = vertexList.addVertex(x1 + (dx + x0 - x1) * WALL_OFFSET, 1, y1 + (dy + y0 - y1) * WALL_OFFSET, u + (1 - WALL_OFFSET), v + 1, 0, 0, 0);
+			int index4 = vertexList.addVertex(x0 + (dx + x1 - x0) * WALL_OFFSET, 1, y0 + (dy + y1 - y0) * WALL_OFFSET, u + (WALL_OFFSET), v + 1, 0, 0, 0);
 
 			vertexList.addIndex(index1, index2, index3);
 			vertexList.addIndex(index1, index3, index4);
@@ -203,26 +217,26 @@ public class MapRenderSystem extends LogicSystem {
 
 			// wall connection
 			if (connect && connect0) {
-				int index1 = vertexList.addVertex(x0, 0, y0, u + 1, v, 1, 1, 1);
-				int index2 = vertexList.addVertex(x0 + (x1 - x0) * WALL_OFFSET, 1, y0 + (y1 - y0) * WALL_OFFSET, u + 1, v + 1, 1, 1, 1);
-				int index3 = vertexList.addVertex(x0 + (dx + x1 - x0) * WALL_OFFSET, 1, y0 + (dy + y1 - y0) * WALL_OFFSET, u + (1 - WALL_OFFSET), v + 1, 1, 1, 1);
+				int index1 = vertexList.addVertex(x0, 0, y0, u + 1, v, 0, 0, 0);
+				int index2 = vertexList.addVertex(x0 + (x1 - x0) * WALL_OFFSET, 1, y0 + (y1 - y0) * WALL_OFFSET, u + 1, v + 1, 0, 0, 0);
+				int index3 = vertexList.addVertex(x0 + (dx + x1 - x0) * WALL_OFFSET, 1, y0 + (dy + y1 - y0) * WALL_OFFSET, u + (1 - WALL_OFFSET), v + 1, 0, 0, 0);
 
 				vertexList.addIndex(index1, index2, index3);
 			}
 
 			if (connect && connect1) {
-				int index4 = vertexList.addVertex(x1, 0, y1, u, v, 1, 1, 1);
-				int index5 = vertexList.addVertex(x1 + (dx + x0 - x1) * WALL_OFFSET, 1, y1 + (dy + y0 - y1) * WALL_OFFSET, u + WALL_OFFSET, v + 1, 1, 1, 1);
-				int index6 = vertexList.addVertex(x1 + (x0 - x1) * WALL_OFFSET, 1, y1 + (y0 - y1) * WALL_OFFSET, u, v + 1, 1, 1, 1);
+				int index4 = vertexList.addVertex(x1, 0, y1, u, v, 0, 0, 0);
+				int index5 = vertexList.addVertex(x1 + (dx + x0 - x1) * WALL_OFFSET, 1, y1 + (dy + y0 - y1) * WALL_OFFSET, u + WALL_OFFSET, v + 1, 0, 0, 0);
+				int index6 = vertexList.addVertex(x1 + (x0 - x1) * WALL_OFFSET, 1, y1 + (y0 - y1) * WALL_OFFSET, u, v + 1, 0, 0, 0);
 
 				vertexList.addIndex(index4, index5, index6);
 			}
 
 			if (door) {
-				int index1 = vertexList.addVertex(x0, 0, y0, u, v, 1, 1, 1);
-				int index2 = vertexList.addVertex(x1, 0, y1, u + 1, v, 1, 1, 1);
-				int index3 = vertexList.addVertex(x1 + (x0 - x1) * WALL_OFFSET, 1, y1 + (y0 - y1) * WALL_OFFSET, u + (1 - WALL_OFFSET), v + 1, 1, 1, 1);
-				int index4 = vertexList.addVertex(x0 + (x1 - x0) * WALL_OFFSET, 1, y0 + (y1 - y0) * WALL_OFFSET, u + (WALL_OFFSET), v + 1, 1, 1, 1);
+				int index1 = vertexList.addVertex(x0, 0, y0, u, v, 0, 0, 0);
+				int index2 = vertexList.addVertex(x1, 0, y1, u + 1, v, 0, 0, 0);
+				int index3 = vertexList.addVertex(x1 + (x0 - x1) * WALL_OFFSET, 1, y1 + (y0 - y1) * WALL_OFFSET, u + (1 - WALL_OFFSET), v + 1, 0, 0, 0);
+				int index4 = vertexList.addVertex(x0 + (x1 - x0) * WALL_OFFSET, 1, y0 + (y1 - y0) * WALL_OFFSET, u + (WALL_OFFSET), v + 1, 0, 0, 0);
 
 				vertexList.addIndex(index1, index2, index3);
 				vertexList.addIndex(index1, index3, index4);
