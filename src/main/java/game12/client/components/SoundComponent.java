b951a9c9cd94edd6b463b975410531362d317609
Nerogar <nerogar@arcor.de>
2018-03-10 17:31:49 +0100

    stop sounds when the entity is removed

--- src/main/java/game12/client/components/SoundComponent.java
+++ src/main/java/game12/client/components/SoundComponent.java
@@ -1,27 +1,48 @@
 package game12.client.components;
 
+import de.nerogar.noise.serialization.NDSException;
+import de.nerogar.noise.serialization.NDSNodeObject;
 import de.nerogar.noise.sound.Sound;
 import game12.client.systems.SoundSystem;
 import game12.core.components.PositionComponent;
 import game12.core.map.Component;
+import game12.core.systems.GameObjectsSystem;
 
 public class SoundComponent extends Component {
 
+	private String soundFile;
 	public Sound sound;
+
+	public SoundComponent() {
+	}
+
+	public SoundComponent(String soundFile) {
+		this.soundFile = soundFile;
+	}
+
+	@Override
+	public void setData(GameObjectsSystem gameObjectsSystem, NDSNodeObject data) throws NDSException {
+		this.soundFile = data.getStringUTF8("soundFile");
+	}
 
 	@Override
 	protected void initSystems() {
 		sound = getEntity().getMap().getSystem(SoundSystem.class).playSound(
-				"res/sound/turretProjectile/flame.ogg",
+				soundFile,
 				getEntity().getComponent(PositionComponent.class).getX(),
 				getEntity().getComponent(PositionComponent.class).getY() + 1.2f,
 				getEntity().getComponent(PositionComponent.class).getZ()
-		                                                                         );
+		                                                                   );
 		sound.setLoop(true);
 	}
 
 	@Override
+	protected void cleanup() {
+		sound.stop();
+	}
+
+	@Override
 	public Component clone() {
-		return new SoundComponent();
+		return new SoundComponent(soundFile);
 	}
 }
