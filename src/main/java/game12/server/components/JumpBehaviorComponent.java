0f17a8685c4ad760c943b1aad5624bedaf8b081a
Nerogar <nerogar@arcor.de>
2018-03-12 02:28:41 +0100

    move jump behavior logic from component into system

--- src/main/java/game12/server/components/JumpBehaviorComponent.java
+++ src/main/java/game12/server/components/JumpBehaviorComponent.java
@@ -2,42 +2,33 @@ package game12.server.components;
 
 import de.nerogar.noise.serialization.NDSException;
 import de.nerogar.noise.serialization.NDSNodeObject;
-import de.nerogar.noise.util.Vector2f;
 import de.nerogar.noise.util.Vector3f;
 import game12.core.EventTimer;
 import game12.core.components.PositionComponent;
 import game12.core.map.Component;
 import game12.core.systems.GameObjectsSystem;
-import game12.core.systems.MapSystem;
-import game12.core.systems.PlayerSystem;
-
-import java.util.Random;
 
 public class JumpBehaviorComponent extends BehaviorComponent {
 
-	private static final float GRAVITY = -20f;
+	public static final float GRAVITY = -20f;
 
 	private float      jumpDelay;
-	private EventTimer jumpTimer;
-	private float      speed;
-	private float      maxDistance;
-	private float      playerProbability;
+	public  EventTimer jumpTimer;
+	public  float      speed;
+	public  float      maxDistance;
+	public  float      playerProbability;
 
-	private Vector3f jumpSource;
-	private Vector3f jumpTarget;
+	public Vector3f jumpSource;
+	public Vector3f jumpTarget;
 
-	private float totalJumpTime;
-	private float jumpProgress;
-	private float jumpX;
-	private float jumpZ;
-	private float jumpParabolaA;
-	private float jumpParabolaB;
+	public float totalJumpTime;
+	public float jumpProgress;
+	public float jumpX;
+	public float jumpZ;
+	public float jumpParabolaA;
+	public float jumpParabolaB;
 
-	private MapSystem    mapSystem;
-	private PlayerSystem playerSystem;
-
-	private Random            random;
-	private PositionComponent positionComponent;
+	public  PositionComponent positionComponent;
 
 	public JumpBehaviorComponent() {
 	}
@@ -53,14 +44,7 @@ public class JumpBehaviorComponent extends BehaviorComponent {
 
 	@Override
 	protected void init() {
-		random = new Random();
 		positionComponent = getEntity().getComponent(PositionComponent.class);
-	}
-
-	@Override
-	protected void initSystems() {
-		mapSystem = getEntity().getMap().getSystem(MapSystem.class);
-		playerSystem = getEntity().getMap().getSystem(PlayerSystem.class);
 	}
 
 	@Override
@@ -69,89 +53,6 @@ public class JumpBehaviorComponent extends BehaviorComponent {
 		this.speed = data.getFloat("speed");
 		this.maxDistance = data.getFloat("maxDistance");
 		this.playerProbability = data.getFloat("playerProbability");
-	}
-
-	private void nextTarget() {
-
-		Vector2f pos = new Vector2f(positionComponent.getX(), positionComponent.getZ());
-		Vector2f playerPos = new Vector2f(playerSystem.getPlayerPosition().getX(), playerSystem.getPlayerPosition().getZ());
-
-		float targetX;
-		float targetY;
-
-		if (pos.subtracted(playerPos).getSquaredValue() < maxDistance * maxDistance && random.nextFloat() < 0.4) {
-			targetX = playerPos.getX();
-			targetY = playerPos.getY();
-		} else {
-			targetX = random.nextFloat() * (maxDistance * 2) - maxDistance + positionComponent.getX();
-			targetY = random.nextFloat() * (maxDistance * 2) - maxDistance + positionComponent.getZ();
-		}
-
-		if (mapSystem.get((int) targetX, (int) targetY) == getOwnRoom() && mapSystem.isWalkable((int) targetX, (int) targetY, false)) {
-			jumpTarget = new Vector3f(targetX, 0.0f, targetY);
-			jumpSource = new Vector3f(positionComponent.getX(), 0.0f, positionComponent.getZ());
-		}
-	}
-
-	public void update(float timeDelta) {
-		jumpTimer.update(timeDelta);
-
-		if (jumpTimer.trigger()) {
-			jumpTarget = null;
-			jumpProgress = 0;
-		}
-
-		if (jumpTarget == null) {
-
-			nextTarget();
-
-			if (jumpTarget != null) {
-				jumpX = jumpTarget.getX() - jumpSource.getX();
-				jumpZ = jumpTarget.getZ() - jumpSource.getZ();
-				float jumpDistance = (float) Math.sqrt(jumpX * jumpX + jumpZ * jumpZ);
-
-				totalJumpTime = jumpDistance / speed;
-				jumpX /= totalJumpTime;
-				jumpZ /= totalJumpTime;
-
-				jumpParabolaA = GRAVITY / 2;
-				jumpParabolaB = -(GRAVITY * totalJumpTime / 2);
-
-				//System.out.println("f(x)=" + jumpParabolaA + "*x^2 + " + jumpParabolaB + "*x");
-
-			}
-		} else {
-			if (jumpProgress > totalJumpTime) {
-				positionComponent.setPosition(
-						jumpTarget.getX(),
-						jumpTarget.getY(),
-						jumpTarget.getZ()
-				                             );
-
-				jumpProgress = totalJumpTime;
-
-			} else if (jumpProgress < totalJumpTime) {
-
-				jumpProgress += timeDelta;
-
-				/*
-				 * parabola ax^2 + bx + c
-				 * with
-				 *   a = gravity / 2
-				 *   b = -(gravity * time / 2)
-				 *   c = 0
-				 */
-
-				float x = jumpProgress;
-
-				positionComponent.setPosition(
-						jumpSource.getX() + jumpX * jumpProgress,
-						jumpSource.getY() + (jumpParabolaA * x * x) + (jumpParabolaB * x),
-						jumpSource.getZ() + jumpZ * jumpProgress
-				                             );
-			}
-		}
-
 	}
 
 	@Override
--- src/main/java/game12/server/systems/JumpBehaviorSystem.java
+++ src/main/java/game12/server/systems/JumpBehaviorSystem.java
@@ -1,17 +1,118 @@
 package game12.server.systems;
 
+import de.nerogar.noise.util.Vector2f;
+import de.nerogar.noise.util.Vector3f;
 import game12.core.event.UpdateEvent;
+import game12.core.systems.MapSystem;
+import game12.core.systems.PlayerSystem;
 import game12.server.components.JumpBehaviorComponent;
 import game12.server.map.ServerMap;
 
+import java.util.Random;
+
 public class JumpBehaviorSystem extends BehaviorSystem<JumpBehaviorComponent> {
+
+	private Random random;
+
+	private PlayerSystem playerSystem;
+	private MapSystem    mapSystem;
 
 	public JumpBehaviorSystem(ServerMap map) {
 		super(JumpBehaviorComponent.class, map);
 	}
 
 	@Override
+	public void init() {
+		super.init();
+
+		this.random = new Random();
+
+		playerSystem = getContainer().getSystem(PlayerSystem.class);
+		mapSystem = getContainer().getSystem(MapSystem.class);
+	}
+
+	private void nextTarget(JumpBehaviorComponent jumpBehavior) {
+
+		Vector2f pos = new Vector2f(jumpBehavior.positionComponent.getX(), jumpBehavior.positionComponent.getZ());
+		Vector2f playerPos = new Vector2f(playerSystem.getPlayerPosition().getX(), playerSystem.getPlayerPosition().getZ());
+
+		float targetX;
+		float targetY;
+
+		if (pos.subtracted(playerPos).getSquaredValue() < jumpBehavior.maxDistance * jumpBehavior.maxDistance && random.nextFloat() < jumpBehavior.playerProbability) {
+			targetX = playerPos.getX();
+			targetY = playerPos.getY();
+		} else {
+			targetX = random.nextFloat() * (jumpBehavior.maxDistance * 2) - jumpBehavior.maxDistance + jumpBehavior.positionComponent.getX();
+			targetY = random.nextFloat() * (jumpBehavior.maxDistance * 2) - jumpBehavior.maxDistance + jumpBehavior.positionComponent.getZ();
+		}
+
+		if (mapSystem.get((int) targetX, (int) targetY) == jumpBehavior.getOwnRoom() && mapSystem.isWalkable((int) targetX, (int) targetY, false)) {
+			jumpBehavior.jumpTarget = new Vector3f(targetX, 0.0f, targetY);
+			jumpBehavior.jumpSource = new Vector3f(jumpBehavior.positionComponent.getX(), 0.0f, jumpBehavior.positionComponent.getZ());
+		}
+	}
+
+	@Override
 	protected void behaviourFunction(UpdateEvent event, JumpBehaviorComponent behaviour) {
-		behaviour.update(event.getDelta());
+		float timeDelta = event.getDelta();
+		behaviour.jumpTimer.update(timeDelta);
+
+		if (behaviour.jumpTimer.trigger()) {
+			behaviour.jumpTarget = null;
+			behaviour.jumpProgress = 0;
+		}
+
+		if (behaviour.jumpTarget == null) {
+
+			nextTarget(behaviour);
+
+			if (behaviour.jumpTarget != null) {
+				behaviour.jumpX = behaviour.jumpTarget.getX() - behaviour.jumpSource.getX();
+				behaviour.jumpZ = behaviour.jumpTarget.getZ() - behaviour.jumpSource.getZ();
+				float jumpDistance = (float) Math.sqrt(behaviour.jumpX * behaviour.jumpX + behaviour.jumpZ * behaviour.jumpZ);
+
+				behaviour.totalJumpTime = jumpDistance / behaviour.speed;
+				behaviour.jumpX /= behaviour.totalJumpTime;
+				behaviour.jumpZ /= behaviour.totalJumpTime;
+
+				behaviour.jumpParabolaA = JumpBehaviorComponent.GRAVITY / 2;
+				behaviour.jumpParabolaB = -(JumpBehaviorComponent.GRAVITY * behaviour.totalJumpTime / 2);
+
+				//System.out.println("f(x)=" + jumpParabolaA + "*x^2 + " + jumpParabolaB + "*x");
+
+			}
+		} else {
+			if (behaviour.jumpProgress > behaviour.totalJumpTime) {
+				behaviour.positionComponent.setPosition(
+						behaviour.jumpTarget.getX(),
+						behaviour.jumpTarget.getY(),
+						behaviour.jumpTarget.getZ()
+				                                       );
+
+				behaviour.jumpProgress = behaviour.totalJumpTime;
+
+			} else if (behaviour.jumpProgress < behaviour.totalJumpTime) {
+
+				behaviour.jumpProgress += timeDelta;
+
+				/*
+				 * parabola ax^2 + bx + c
+				 * with
+				 *   a = gravity / 2
+				 *   b = -(gravity * time / 2)
+				 *   c = 0
+				 */
+
+				float x = behaviour.jumpProgress;
+
+				behaviour.positionComponent.setPosition(
+						behaviour.jumpSource.getX() + behaviour.jumpX * behaviour.jumpProgress,
+						behaviour.jumpSource.getY() + (behaviour.jumpParabolaA * x * x) + (behaviour.jumpParabolaB * x),
+						behaviour.jumpSource.getZ() + behaviour.jumpZ * behaviour.jumpProgress
+				                                       );
+			}
+		}
+
 	}
 }
